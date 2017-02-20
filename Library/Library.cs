86cc8cab6b801351e9bb51ee5a8643809e826811
Jake <necropyre@outlook.com>
2017-02-19 19:41:21 -0700

    Completed Path Checker

--- Library/Library.cs
+++ Library/Library.cs
@@ -1,25 +1,92 @@
 ﻿using System;
 using System.IO;
-
+using System.Security;
 
 namespace Library
 {
     public class Library
     {
+        /// <summary>
+        /// Gets a value that tells whether dirPath is a valid path
+        /// </summary>
+        /// <param name="dirPath"></param>
+        /// <returns></returns>
+        /// 
+        public static bool IsValidPath(string dirPath)
+        {
+            string result;
+            return CheckPath(dirPath, out result);
+        }
 
-        //Checks user entered directory path
-        public bool CheckPath(string userPath)
+        /// <summary>
+        /// Returns absolute path for dirPath (user defined) string. A
+        /// return value will indicate if the path is valid or not.
+        /// </summary>
+        /// <param name="dirPath"></param> User specified path
+        /// <param name="result"></param> 
+        /// <returns>true</returns> if path is correct, otherwise false
+        /// 
+        public static bool CheckPath(string dirPath, out string result)
+        {
+            result = String.Empty;
+            if (String.IsNullOrWhiteSpace(dirPath))
+            {
+                return false;
+            }
+            bool status = false;
+
+            try
+            {
+                result = Path.GetFullPath(dirPath);
+                status = true;
+            }
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Bad Arguments");
+            }
+
+            catch (SecurityException)
+            {
+                Console.WriteLine("File is read only.");
+            }
+
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("Not supported.");
+            }
+
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("Path is too long.");
+            }
+            return status;
+        }
+
+
+        //Find the files and directories that have changed
+        public static void FileWatcher()
+        {
+
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            //fsw.Path = dirPath;
+        }
+
+
+        /*  DEPRECATED
+        
+        public bool CheckPath2(string userPath)
         {
             char[] invalidPathChars;
             int invalidCharPos = -1;
             bool endOfPath = false;
             bool invalidPath = false;
-
+        
             while (!endOfPath)
             {
-                invalidPathChars = Path.GetInvalidPathChars();
-                invalidCharPos = invalidCharPos + 1;
-
+              invalidPathChars = Path.GetInvalidPathChars();
+              invalidCharPos = invalidCharPos + 1;
+        
                 if (invalidCharPos == -1)
                 {
                     endOfPath = true;
@@ -28,7 +95,7 @@ namespace Library
                 {
                     invalidPath = true;
                     Console.WriteLine("Invalid path. Please double check it!");
-
+        
                     if (invalidCharPos >= userPath.Length - 1)
                     {
                         endOfPath = true;
@@ -41,17 +108,6 @@ namespace Library
             }
             return (invalidPath);
         }
-
-        //Find the files and directories that have changed
-        public static void FileWatcher()
-        {
-            
-            FileSystemWatcher fsw = new FileSystemWatcher();
-            //fsw.Path = dirPath;
-        }
-
-
-
-
+        */
     }
 }
