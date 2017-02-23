0f761c7cf64c7bb061cdca4d0da1ffff5582d527
Milo <johnson_milo@hotmail.com>
2017-02-22 19:22:39 -0800

    Added button1 again test

--- DirSync/frmMain.Designer.cs
+++ DirSync/frmMain.Designer.cs
@@ -39,6 +39,7 @@
             this.toLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // okButton
@@ -131,10 +132,20 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AccessibleDescription = "";
             this.ClientSize = new System.Drawing.Size(498, 180);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.toLabel);
@@ -163,6 +174,7 @@
         private System.Windows.Forms.Label toLabel;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
